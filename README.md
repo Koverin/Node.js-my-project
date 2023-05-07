01bc922c4fb293399a38818723c23e33618a0055
Koverin <110396064+Koverin@users.noreply.github.com>
2023-05-07 16:16:53 +0300

    update file

---
+++ README.md
@@ -0,0 +1,12 @@
+1.Получаем и выводим весь список контактов в виде таблицы (console.table)
+node index.js --action list
+https://monosnap.com/file/ldRiFSxXGMISMZZBdkJysux2kCIFre
+2.Получаем контакт по id
+node index.js --action get --id 05olLMgyVQdWRwgKfg5J6
+https://monosnap.com/file/Hg5Lgjtof0ulApYgq0PzDlDIQpOP04
+3.Добавялем контакт
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+https://monosnap.com/file/XGPDwhgLV477PVmGqqcgrNXfsMGNUd
+4.Удаляем контакт
+node index.js --action remove --id qdggE76Jtbfd9eWJHrssH
+https://monosnap.com/file/mblc7qTVT18t4bgTNvwQr1emPH7K8H
--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hello my new world");
+const argv = require("yargs").argv;
+const contacts = require("./db/contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+      const newContacts = await contacts.addContact(name, email, phone);
+      console.log(newContacts);
+      break;
+
+    case "remove":
+      const deletedContact = await contacts.removeContact(id);
+      console.log(deletedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
